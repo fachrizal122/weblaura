ae3854a5928ce1a08035531412fa27c028ca4e96
fachrizal122 <61828332+fachrizal122@users.noreply.github.com>
2024-02-16 14:26:20 +0800

    Add files via upload

---
+++ register.php
@@ -0,0 +1,99 @@
+<?php
+	@ob_start();
+	session_start();
+	if(isset($_POST['proses'])){
+		require 'config.php';
+			
+		$user = strip_tags($_POST['user']);
+		$pass = strip_tags($_POST['pass']);
+
+		$sql = 'select member.*, login.user, login.pass
+				from member inner join login on member.id_member = login.id_member
+				where user =? and pass = md5(?)';
+		$row = $config->prepare($sql);
+		$row -> execute(array($user,$pass));
+		$jum = $row -> rowCount();
+		if($jum > 0){
+			$hasil = $row -> fetch();
+			$_SESSION['admin'] = $hasil;
+			echo '<script>alert("Login Sukses");window.location="index.php"</script>';
+		}else{
+			echo '<script>alert("Login Gagal");history.go(-1);</script>';
+		}
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Register</title>
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="sb-admin/css/sb-admin-2.min.css" rel="stylesheet">
+</head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-7">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Register</h3></div>
+                                    <div class="card-body">
+                                        <form>
+                                            <div class="row mb-3">
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" />                                                        
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating">
+                                                        <input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" />                                                        
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
+                                            </div>
+                                            <div class="row mb-3">
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputPassword" type="password" placeholder="Create a password" />
+
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputPasswordConfirm" type="password" placeholder="Confirm password" />                                                        
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="mt-4 mb-0">                                              
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer text-center py-3">
+                                        <div class="small"><a href="login.php">Have an account? Go to login</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
